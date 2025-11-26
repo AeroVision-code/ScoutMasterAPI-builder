61f96398a01d60997c093ded8928f8a35760b5fb
BioScopeDocu <rinus.vijftigschild@aerovision.nl>
2025-11-26 14:24:46 +0100

    add cultivations

--- scoutmaster/api.py
+++ scoutmaster/api.py
@@ -6,7 +6,8 @@ from .layertypes import LayerTypes
 from .layers import Layers
 from .files import Files
 from .observations import Observations
+from .cultivations import Cultivations
 
-class ScoutMasterAPI(BaseAPI, Projects, Fields, Crops, Layers, LayerTypes, Files, Observations):
+class ScoutMasterAPI(BaseAPI, Projects, Fields, Crops, Layers, LayerTypes, Files, Observations, Cultivations):
     """Aggregates all topic classes into a single API object"""
     pass
---
+++ scoutmaster/cultivations.py
@@ -0,0 +1,10 @@
+class Cultivations:
+    def cultivations(self, project_id):
+        endpoint = f"projects/{project_id}/cultivations"
+        params = {}
+        if self.output_format in ["geojson", "gdf"]:
+            params["output"] = "geojson"
+
+        # Pass params to _get
+        data = self._get(endpoint, params=params)
+        return self._format_output(data)
