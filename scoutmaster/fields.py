35d626dae556a4fc6f08d2ec8bf6217f3a9da341
BioScopeDocu <rinus.vijftigschild@aerovision.nl>
2025-11-27 11:21:27 +0100

    create field

--- scoutmaster/fields.py
+++ scoutmaster/fields.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import requests
+
+
 class Fields:
     def fields(self, project_id):
         endpoint = f"projects/{project_id}/fields"
@@ -8,3 +12,34 @@ class Fields:
         # Pass params to _get
         data = self._get(endpoint, params=params)
         return self._format_output(data)
+    
+    def fields_create(self, project_id, field_data):
+        """
+        Create a new observation in the specified project.
+
+        Args:
+            project_id (str): The ID of the project.
+            obs_data (dict): Observation data with required fields:
+                {
+                    "user_id": "uuid",
+                    "name": "string"
+                    "properties": {
+                        "des"
+                    },
+                    "geometry": { "type": "Polygon, "coordinates": [[x, y]] },
+                    "reference_code": "string"
+                }
+
+        Returns:
+            pd.DataFrame or dict: Created observation.
+        """
+        self._check_auth()
+        
+        endpoint = f"projects/{project_id}/fields"
+        try:
+            data = self._post(endpoint, field_data)
+            print(data)
+            return pd.DataFrame(data) if self.output_format == "df" else data
+
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Request failed: {e}")
