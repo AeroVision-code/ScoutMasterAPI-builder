6235a9a548085797964a248676216b0024f6ea90
BioScopeDocu <rinus.vijftigschild@aerovision.nl>
2025-11-27 11:36:05 +0100

    users

--- scoutmaster/api.py
+++ scoutmaster/api.py
@@ -7,7 +7,8 @@ from .layers import Layers
 from .files import Files
 from .observations import Observations
 from .cultivations import Cultivations
+from .users import Users
 
-class ScoutMasterAPI(BaseAPI, Projects, Fields, Crops, Layers, LayerTypes, Files, Observations, Cultivations):
+class ScoutMasterAPI(BaseAPI, Projects, Fields, Crops, Layers, LayerTypes, Files, Observations, Cultivations, Users):
     """Aggregates all topic classes into a single API object"""
     pass
