ceccb8dfe9576d43df0d0f7b3ce0a6ef28f0e60b
BioScopeDocu <rinus.vijftigschild@aerovision.nl>
2025-12-01 10:43:34 +0100

    cultivations by id

--- scoutmaster/cultivations.py
+++ scoutmaster/cultivations.py
@@ -8,3 +8,13 @@ class Cultivations:
         # Pass params to _get
         data = self._get(endpoint, params=params)
         return self._format_output(data)
+    def cultivations_by_field(self, field_id):
+        endpoint = f"fields/{field_id}/calendar"
+        params = {}
+        if self.output_format in ["geojson", "gdf"]:
+            params["output"] = "geojson"
+
+        # Pass params to _get
+        data = self._get(endpoint, params=params)
+        return self._format_output(data)
+    
