6105413a5ea6292071d5b67353f0bebba0f04444
BioScopeDocu <rinus.vijftigschild@aerovision.nl>
2025-12-09 20:22:02 +0100

    fix project create

--- scoutmaster/projects.py
+++ scoutmaster/projects.py
@@ -26,17 +26,11 @@ class Projects:
         project_data = {"user_id": user_id,
                   "name": name,
                   "abbreviation": abbreviation}
-        headers = {
-            'Authorization': f'Bearer {self.access_token}',
-            'Content-Type': 'application/json'
-        }
         try:
-            response = requests.post(endpoint, json=project_data, headers=headers)
-            response_json = response.json()
-            if response.status_code == 200 or response.status_code == 201:
-                return pd.DataFrame(response_json) if self.output_format == "df" else response_json
-            else:
-                raise Exception(f"Failed to create fields: {response.status_code} {response.text}")
+            data = self._post(endpoint, project_data)
+            print(data)
+            return pd.DataFrame(data) if self.output_format == "df" else data
+
         except requests.exceptions.RequestException as e:
             raise Exception(f"Request failed: {e}")
         
