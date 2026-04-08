099492898a06232b795292994e6fb2851f8f0162
Hoek <steven.hoek@wur.nl>
2026-04-08 16:18:23 +0200

    Extra request types were added to the code in order to facilitate data retrieval

--- scoutmasterapi_builder/fields.py
+++ scoutmasterapi_builder/fields.py
@@ -4,6 +4,13 @@ import requests
 
 class Fields:
     def fields(self, project_id):
+        """
+        Get all the fields that are used within the specified project.
+        Args:
+            project_id (str): The ID of the project
+        Returns:
+            pd.DataFrame or list: Fields as DataFrame or JSON list.
+        """
         endpoint = f"projects/{project_id}/fields"
         params = {}
         if self.output_format in ["geojson", "gdf"]:
@@ -14,6 +21,13 @@ class Fields:
         return self._format_output(data)
     
     def field_by_id(self, field_id):
+        """
+        Get the field indicated by the given field ID.
+        Args:
+            field_id (str): The ID of the field
+        Returns:
+            pd.DataFrame or list: Field as DataFrame or JSON list.
+        """
         endpoint = f"fields/{field_id}"
         params = {}
         if self.output_format in ["geojson", "gdf"]:
@@ -23,6 +37,26 @@ class Fields:
             data = [data]
         return self._format_output(data)
     
+    def field_by_location(self, project_id, lat, lon):
+        """
+        Get the field used within the specified project, filtered by the point location.
+        Args:
+            project_id (str): The ID of the project
+            lat (float): the latitude of the point
+            lon (float): the longitude of the point
+        Returns:
+            pd.DataFrame or list: Field as DataFrame or JSON list.
+        """
+        endpoint = f"projects/{project_id}/fields"
+        endpoint += f"/by-location?lat={lat}&long={lon}"
+        params = {}
+        if self.output_format in ["geojson", "gdf"]:
+            params["output"] = "geojson"
+        data = self._get(endpoint, params=params)
+        if self.output_format in ["df"]:
+            data = [data]
+        return self._format_output(data)
+        
     def fields_create(self, project_id, field_data):
         """
         Create a new field in the specified project.
