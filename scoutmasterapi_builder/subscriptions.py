20186d433ec9ff7edb6393ad62fa41839d7526b5
StevenHoek <steven.hoek@aerovision.nl>
2026-05-06 11:26:14 +0200

    Comments added

--- scoutmasterapi_builder/subscriptions.py
+++ scoutmasterapi_builder/subscriptions.py
@@ -1,5 +1,12 @@
 class Subscriptions:
     def subscriptions_by_field(self, field_id):
+        """
+        Get all the subscriptions which are related to a certain field
+        Args:
+            field_id (str): The ID of the field
+        Returns:
+            pd.DataFrame or list: DataFrame or JSON list with the relevant data
+        """
         endpoint = f"subscription/{field_id}/"
         params = {}
         if self.output_format in ["geojson", "gdf"]:
--- scoutmasterapi_builder/users.py
+++ scoutmasterapi_builder/users.py
@@ -1,5 +1,12 @@
 class Users:    
     def users(self, project_id):
+        """
+        Get all users who have access to the project.
+        Args:
+            project_id (str): The ID of the project
+        Returns:
+            pd.DataFrame or list: Users as DataFrame or JSON list.
+        """
         endpoint = f"projects/{project_id}/users"
         params = {}
         if self.output_format in ["geojson", "gdf"]:
